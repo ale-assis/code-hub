213661d7eb7519215d6762bdeda832fc061f49cf
ale-assis <alexandre7assis@gmail.com>
2025-08-18 19:04:21 -0300

    add: exercism folder and ex.1

---
+++ 0. Dojô de Lógica/EXERCISM/hello-world/hello_world.py
@@ -0,0 +1,8 @@
+"""EX 1: Printando Hello World através de uma função"""
+
+def hello():
+    """Printa a string 'Hello World'"""
+    return 'Hello, World!'
+
+hello_world = hello()
+print(hello_world)
