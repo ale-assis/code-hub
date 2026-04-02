fd0da1db53f4d1fe38307da54c87589a9467dfb8
Alexandre Assis <alexandre7assis@gmail.com>
2026-04-02 13:04:52 -0300

    feat: adiciona módulo 01 nos snippets de python

---
+++ 1. Python HUB/00. Snippets/01. Executando o código/01. executar sem print.py
@@ -0,0 +1,15 @@
+numero = 1
+numero + 2
+
+= 3
+
+
+'''
+
+% Para executar o código eu sempre preciso usar print() ? 
+- não, você pode digitar uma operação no python e executar o 
+código que ele vai rodar 
+no entanto, somente a última linha do código que vai ter o 
+seu resultado exibido (output) nesse caso foi o 3
+
+'''
