3e324fd3df48245134d14b14a3a27e4cb6ebe417
Alexandre Assis <alexandre7assis@gmail.com>
2026-04-02 13:15:28 -0300

    feat: adiciona módulo 02 nos snippets de python

---
+++ 1. Python HUB/00. Snippets/02. Comentários/01. comentando linhas.py
@@ -0,0 +1,8 @@
+# pip install pandas --user 
+#pip install pandas --user 
+
+'''
+coloca o # no começo da linha 
+Tanto faz ter espaço depois do # ou não 
+
+'''
---
+++ 1. Python HUB/00. Snippets/02. Comentários/02. comentando um bloco inteiro de código.py
@@ -0,0 +1,26 @@
+# forma 1
+
+'''
+for i in range(10):
+    if(i < limiar):
+        menores.append(i)
+    elif(i > limiar):
+        maiores.append(i)
+
+    print('Resultado final:')
+    print('menores:', menores)
+    print('maiores:', maiores
+'''
+
+
+# forma 2
+
+'''for i in range(10):
+    if(i < limiar):
+        menores.append(i)
+    elif(i > limiar):
+        maiores.append(i)
+
+    print('Resultado final:')
+    print('menores:', menores)
+    print('maiores:', maiores'''
